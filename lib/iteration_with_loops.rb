0314b0064299485c48f16914aa34686e7631ae81
maxrabin <max.rabin17@gmail.com>
2021-06-25 13:07:50 -0400

    commit tree bd5253f6dbd3e5523217e254a00a224df9abe5b6

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,22 @@
+require 'pry'
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  
+  # src.join(", ")
+  # new_array = src.map do |array| 
+  #   array.filter {|x| x.class == String}
+  # end
+  # new_array.join(" ")
+  
+  # src.flatten().filter{|element| element.class == String}.join(" ")
+
+  src.flatten().reduce("") do |accum, element|
+    if element.class == String
+      accum + " " + element
+    else 
+      accum
+      # inside else 'element' is a number, so we don't do anything with it
+    end
+  end
 end
